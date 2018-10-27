3e391af83a7f7678cf9db88d8d7f1afe06a91a5e
Yasmin Torres <yasmintorres90@hotmail.com>
2018-10-27 23:14:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 
 def valid_move?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
+  if board[index] == " " || board[index] == "" || board[index] == false
     return true
   else
     return false
